8aea51bf4dd2b3d6d87c775040d5db1513eb8146
Evan Munnik <evanmunnik01@gmail.com>
2026-03-26 21:52:08 +0200

    Updated main directory and file content

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
 // Select ALL input fields
-const inputFields = document.querySelectorAll("input");
+const form = document.querySelector("form");
+const allInputs = document.querySelectorAll("input");
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const errorMessageArr = document.querySelectorAll(".error-message");
 const submitBtn = document.getElementById("submit-btn");
-
-firstName.classList.add("error");
 
 // Object of types of error messages
 const errors = {
@@ -16,4 +18,41 @@ const errors = {
   password: "Password cannot be empty",
 };
 
-// Create a database to validate user logins - Add value Section
+// Add event to the submit button
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let formValid = false;
+
+  allInputs.forEach((el) =>
+    el.value === "" ? el.classList.add("error") : el.classList.remove("error"),
+  );
+
+  errorMessageArr.forEach((el) => {
+    if (el.previousElementSibling.classList.contains("error")) {
+      if (el.previousElementSibling.getAttribute("id") === "email") {
+        email.classList.add("email-placeholder");
+        email.placeholder = "example@email.com";
+      }
+
+      el.style.display = "block";
+      el.textContent =
+        errors[`${el.previousElementSibling.getAttribute("id")}`];
+    } else {
+      el.style.display = "none";
+      form.submit;
+    }
+  });
+
+  if (!email.checkValidity()) {
+    email.classList.add("error");
+    email.nextElementSibling.style.display = "block";
+  }
+
+  Array.from(allInputs).some((input) => {
+    input.classList.contains("error")
+      ? (formValid = false)
+      : (formValid = true);
+  });
+
+  formValid ? form.submit("Form Submitted") : "";
+});
